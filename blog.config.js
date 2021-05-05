8393afa43c99d83a444772201abfb18dcd08fd0b
felixmon <37103200+felixmon@users.noreply.github.com>
2021-05-05 17:28:52 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 const BLOG = {
   title: 'CRAIGARY',
-  author: 'Craig Hart',
+  author: 'Felix',
   email: 'i@craigary.net',
   link: 'https://nobelium.vercel.app',
   description: 'This gonna be an awesome website.',
